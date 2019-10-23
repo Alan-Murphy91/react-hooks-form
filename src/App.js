f3f18492aa7fad0283f07d3a5b20148d05aa8004
Alan <alanjmurphy91@gmail.com>
2019-10-23 12:09:28 +0100

    babel and form

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Form from './Form';
+
+const App = () => 
+		<div className="App">
+			<Form />
+   		</div>
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './redux/store/configureStore';
+// import './index.css';
+import App from './App';
+
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
-// const store = configureStore();
-
-// const App = (
-// 	<Provider store={store}>
-// 		<AppRouter />
-// 	</Provider>
-// );
-
-const App = <AppRouter />
+import 'bulma/css/bulma.css';
 
 ReactDOM.render(
-	App, 
+	<App />, 
 	document.getElementById('app')
 );
