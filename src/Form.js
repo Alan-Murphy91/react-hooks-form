7dee7c72f99bafb84f760798805d1817540e471c
Alan <alanjmurphy91@gmail.com>
2019-10-23 12:44:24 +0100

    form hook

--- src/Form.js
+++ src/Form.js
@@ -1,28 +1,90 @@
 import React from 'react';
+import useForm from './useForm';
 
-const Form = () =>
-	<div className="section is-fullheight">
-		<div className="container">
-			<div className="column is-4 is-offset-4">
-				<div className="box">
-					<form>
-						<div className="field">
-							<label className="label">Email Address</label>
-							<div className="control">
-								<input className="input" type="email" name="email" required />
+const Form = () => {
+	const { values, handleChange, handleSubmit } = useForm();
+	return (
+		<div className="section is-fullheight">
+			<div className="container">
+				<div className="column is-4 is-offset-4">
+					<div className="box">
+						<form onSubmit={handleSubmit}>
+							<div className="field">
+								<label className="label">Email Address</label>
+								<div className="control">
+									<input className="input" type="email" name="email" onChange={handleChange} value={values.email} required />
+								</div>
 							</div>
-						</div>
-						<div className="field">
-							<label className="label">Password</label>
-							<div className="control">
-								<input className="input" type="password" name="password" required />
+							<div className="field">
+								<label className="label">Password</label>
+								<div className="control">
+									<input className="input" type="password" name="password" onChange={handleChange} value={values.password} required />
+								</div>
 							</div>
-						</div>
-						<button type="submit" className="button is-block is-info is-fullwidth">Login</button>
-					</form>
+							<button type="submit" className="button is-block is-info is-fullwidth">Login</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	);
+}
 
 export default Form;
+
+
+// The below is the same but using a class based Component
+
+// import React, { Component } from 'react';
+
+// class Form extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: '',
+//       password: '',
+//     };
+//   }
+
+//   handleChange(event) {
+//     this.setState({
+//       [event.target.name]: event.target.value,
+//     });
+//   }
+
+//   login() {
+//     const { email, password } = this.state;
+//     console.log(`${email}, ${password}`);
+//   }
+
+//   render() {
+//     const { email, password } = this.state;
+//     return (
+//       <div className="section is-fullheight">
+//         <div className="container">
+//           <div className="column is-4 is-offset-4">
+//             <div className="box">
+//               <form onSubmit={this.login}>
+//                 <div className="field">
+//                   <label className="label">Email Address</label>
+//                   <div className="control">
+//                     <input className="input" type="email" name="email" onChange={this.handleChange} value={email || ''} required />
+//                   </div>
+//                 </div>
+//                 <div className="field">
+//                   <label className="label">Password</label>
+//                   <div className="control">
+//                     <input className="input" type="password" name="password" onChange={this.handleChange} value={password || ''} required />
+//                   </div>
+//                 </div>
+//                 <button type="submit" className="button is-block is-info is-fullwidth">Login</button>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Form;
